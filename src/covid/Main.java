0ad2037f1987eb1d1814c34b6fb5d283b26821e5
Minh Vu <minh.duplo@gmail.com>
2021-08-16 18:21:31 +0700

    Modified parsing and tested

--- src/covid/Main.java
+++ src/covid/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        List<Row> test = csvReader.readCSV("C:\\Users\\minhd\\IdeaProjects\\GroupAssignmentJava\\src\\covid\\covid-data.csv");
+        List<Row> test = csvReader.readCSV("src/covid/covid-data.csv");
         System.out.println(test);
     }
 }
